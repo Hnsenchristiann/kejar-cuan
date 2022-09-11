3f32631909c66639346c49c2d426edc3b35c6e7c
hnsenchristiann <christiansensen00@gmail.com>
2022-09-11 21:29:01 +0700

    start

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav> -->
+  <div class="bg-black h-screen">
+    <nav-bar />
+    <router-view/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from '@/components/NavBar.vue'
 
 export default {
-  name: 'App',
+  name: 'HomeView',
   components: {
-    HelloWorld
+    NavBar
   }
 }
 </script>
@@ -21,6 +27,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <div class="text-left text-white px-3">
+      <div class="text-2xl font-bold">
+        TERJUN KE '<a class="italic">DUNIA KERJA</a>' TANPA PANDANG USIA
+      </div>
+      <div class="text-sm">Belajar cari peluang di usia muda, dan segera dapatkan pengalaman kerja.</div>
+      <div class="pt-2">
+        <button class="bg-green-600 rounded-full px-3 py-1">Lebih Lanjut</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
